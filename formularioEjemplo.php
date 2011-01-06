bb8c45881e89d142ad4654bed130205c3c9fd098
irtusb <irtusb@gmail.com>
2011-01-06 00:03:25 -0300

    un ejemplo de cómo obtener los datos que pone el usuario SIN AJAX
    Signed-off-by: irtusb <irtusb@gmail.com>

---
+++ formularioEjemplo.php
@@ -0,0 +1,17 @@
+<?php
+print $_POST['cajaTexto'];
+print "\n";
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+  <head>
+  <meta http-equiv="content-type" content="text/html; charset=utf-8">
+  <title>título de la custión</title>
+  </head>
+  <body>
+    <form name="formulario" action="" method="post">
+      <input type="text" name="cajaTexto">
+      <input type="submit" value="enviar">
+    </form>
+  </body>
+</html>
